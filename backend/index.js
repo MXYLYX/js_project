623e04e1f8466c40063553724db15c973a095a0f
Athos <1362563840@qq.com>
2021-08-04 07:22:10 +0800

    node_module

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+
+// const { response } = require('express');
+const express = require('express');
+// const { request } = require('http');
+// 5. 导入querystring模块（用来解析post请求参数）
+var querystring = require('querystring');
+var fs = require('fs');  
+
+const app = express();
+
+app.get('/', (request, response) =>{
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    // console.log(request);
+    // query只适用于get
+    console.log(`打印`);
+    fs.readFile('../frontend/index.html',function(err,data){  
+        if(err){  
+            throw err;  
+        }  
+        response.end(data);  
+    });  
+
+});
+
+const https = require('https')
+// https.createServer(app).listen(443)
+
+app.listen(2000, ()=>{
+    console.log(`启动监听`);
+});
+
