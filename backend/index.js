ae1fef4ba05ae129480c2c55c5640daf9c11e7e5
MXYLYX <88269845+MXYLYX@users.noreply.github.com>
2021-08-04 09:22:15 +0800

    prject根目录下有txt文件 以后找不到express moduel 都用这个办法

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.get('/', (request, response) =>{
 const https = require('https')
 // https.createServer(app).listen(443)
 
-app.listen(2000, ()=>{
+app.listen(80, ()=>{
     console.log(`启动监听`);
 });
 
