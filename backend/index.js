db1cbe9b9189ec9c1f46b6fd80db741f02a6ac24
MXYLYX <88269845+MXYLYX@users.noreply.github.com>
2021-09-30 17:43:11 +0800

    测试cookie

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ const express = require('express');
 // const { request } = require('http');
 // 5. 导入querystring模块（用来解析post请求参数）
 var querystring = require('querystring');
-var fs = require('fs');  
+var fs = require('fs'); 
+const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, './frontend'))); 
 
 app.get('/', (request, response) =>{
     response.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,7 +25,26 @@ app.get('/', (request, response) =>{
 
 });
 
-const https = require('https')
+app.get('/axios', (request, response) =>{
+    console.log('headers: ', request.headers);
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    // 当带有Access-Control-Allow-Credentials时 Access-Control-Allow-Origin不能为*
+    response.setHeader('Access-Control-Allow-Credentials', true); 
+    console.log(request.headers);
+    // query只适用于get
+    console.log(`测试cookie`);
+    if(!request.query.name){
+        console.log(`不包含name`);
+    }else{
+        console.log(request.query.name);
+    }
+
+    response.send('hello axios jquert get');
+});
+
+
+// const https = require('https')
 // https.createServer(app).listen(443)
 
 app.listen(process.env.PORT || 80, ()=>{
